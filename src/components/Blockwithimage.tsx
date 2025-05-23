4cdf83651e9a090b25dfe12c81bb248ae678edd0
Ricardo Arroba Test <ricardo@test.com>
2025-05-23 21:06:45 +0000

    Update site

---
+++ src/components/Blockwithimage.tsx
@@ -0,0 +1,23 @@
+export function Blockwithimage(props: any) {
+  return (
+    <section className="text-gray-600 body-font">
+      <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+        {props.t1}
+        <div className="text-center lg:w-2/3 w-full">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+            {props.t2}
+          </h1>
+          <p className="mb-8 leading-relaxed">{props.t3}</p>
+          <div className="flex justify-center">
+            <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+              {props.t4}
+            </button>
+            <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+              {props.t5}
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,27 @@
 import Head from "next/head";
+import { Blockwithimage } from "@/components/Blockwithimage";
+import Image from "next/image";
 export default function Page() {
   return (
     <>
       <Head>
         <title>Home</title>
       </Head>
-      <></>
+      <Blockwithimage
+        t1={
+          <Image
+            src="/Syndra_54.jpg"
+            width={1215}
+            height={717}
+            alt="hero"
+            className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
+          />
+        }
+        t2="Microdosing synth tattooed vexillologist"
+        t3="Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork."
+        t4="Button"
+        t5="Button"
+      />
     </>
   );
 }
